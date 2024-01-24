b397e7bb66deb911cab2d2ac7910ed0ea219fc54
edenb27 <edenblavat@gmail.com>
2024-01-24 11:24:02 +0200

    Final

--- metricStreamer.py
+++ metricStreamer.py
@@ -1,41 +1,63 @@
 import boto3
 import json
-import time
-def lambda_handler(event, context):
-    sqs_client = boto3.resource('sqs', region_name='us-east-2')
-    asg_client = boto3.client('autoscaling', region_name='us-east-2')
-    cw_client = boto3.client('cloudwatch', region_name='us-east-2')
 
+region_name = 'us-east-2'
+
+
+def lambda_handler(event, context):
+    # AWS configuration
     AUTOSCALING_GROUP_NAME = 'edenb27-yolo-AS'
     QUEUE_NAME = 'edenb-yolo5'
+    NAMESPACE = 'edenb27-AWS-metric'
+    METRIC_NAME = 'BacklogPerInstance'
 
-    queue = sqs_client.get_queue_by_name(QueueName=QUEUE_NAME)
-    msgs_in_queue = int(queue.attributes.get('ApproximateNumberOfMessages'))
-    asg_groups = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[AUTOSCALING_GROUP_NAME])[
-        'AutoScalingGroups']
+    # Initialize AWS clients
+    sqs_client = boto3.client('sqs', region_name=region_name)
+    asg_client = boto3.client('autoscaling', region_name=region_name)
+    cloudwatch_client = boto3.client('cloudwatch', region_name=region_name)
 
-    if not asg_groups:
-        raise RuntimeError('Autoscaling group not found')
-    else:
-        asg_size = asg_groups[0]['DesiredCapacity']
+    try:
+        # Get the number of messages in the SQS queue
+        queue_url = f'https://sqs.us-east-2.amazonaws.com/352708296901/edenb-yolo5'
+        response = sqs_client.get_queue_attributes(
+            QueueUrl=queue_url,
+            AttributeNames=['ApproximateNumberOfMessages']
+        )
+        msgs_in_queue = int(response['Attributes']['ApproximateNumberOfMessages'])
 
-# TODO send backlog_per_instance to cloudwatch...
-    if msgs_in_queue and asg_size == 0:
-        pass
-    else:
-        backlog_per_instance = msgs_in_queue / asg_size
-        put_metric = cw_client.put_metric_data(
-            Namespace='edenb27-AWS-metric',
+        # Get the desired capacity of the Auto Scaling Group
+        asg_response = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[AUTOSCALING_GROUP_NAME])
+        if not asg_response['AutoScalingGroups']:
+            raise RuntimeError('Autoscaling group not found')
+        asg_size = asg_response['AutoScalingGroups'][0]['DesiredCapacity']
+
+        print(f'Messages in Queue: {msgs_in_queue}')
+        print(f'Desired Capacity of ASG: {asg_size}')
+
+        # Avoid division by zero
+        if asg_size == 0:
+            backlog_per_instance = msgs_in_queue
+        else:
+            backlog_per_instance = msgs_in_queue / asg_size
+
+        # Send the metric to CloudWatch
+        response = cloudwatch_client.put_metric_data(
+            Namespace=NAMESPACE,
             MetricData=[
                 {
-                'MetricName': 'BacklogPerInstance',
-                'Value': backlog_per_instance,
-                'Unit': 'None'
-                },
-             ]
+                    'MetricName': METRIC_NAME,
+                    'Value': backlog_per_instance,
+                    'Unit': 'None'
+                }
+            ]
         )
 
+        print(f'BacklogPerInstance: {backlog_per_instance}')
+
+    except Exception as e:
+        print(f'Error: {str(e)}')
+
     return {
-        'status Code': 200,
-        'body': json.dumps(put_metric)
+        'statusCode': 200,
+        'body': json.dumps(response)
     }
